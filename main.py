9c537e8ce26eb437a9e55fbad90436ea82683bf7
Untitled1234523 <kent.spike24x7@gmail.com>
2023-12-09 11:03:29 +0300

    Правки

--- main.py
+++ main.py
@@ -5,18 +5,15 @@ from dotenv import load_dotenv, find_dotenv
 import time
 
 
-load_dotenv(find_dotenv())
-
-
-def timer(chat_id, question):
+def timer(chat_id, question, bot):
     delay = parse(question)
     message_id = bot.send_message(chat_id, "Таймер запущен!!!")
-    bot.create_countdown(delay, counting, chat_id=chat_id, message_id=message_id, delay=delay)
+    bot.create_countdown(delay, counting, chat_id=chat_id, message_id=message_id, delay=delay, bot=bot)
     time.sleep(delay+0.5)
     bot.send_message(chat_id, 'Время вышло!!!')
 
 
-def counting(second_left, chat_id, message_id, delay):
+def counting(second_left, chat_id, message_id, delay, bot):
     bot.update_message(chat_id, message_id, "Осталось секунд: {}\n ".format(second_left) + render_progressbar(delay, delay - second_left))
 
 
@@ -29,9 +26,11 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-bot = ptbot.Bot(os.getenv('TG_TOKEN'))
-bot.reply_on_message(timer)
-bot.run_bot()
+def main():
+    load_dotenv(find_dotenv())
+    bot = ptbot.Bot(os.getenv('TG_TOKEN'))
+    bot.reply_on_message(timer, bot=bot)
+    bot.run_bot()
 
 
 if __name__ == '__main__':
